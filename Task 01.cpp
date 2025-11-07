8583d5d4d8d43fb61d72c2e27b2aa4aac820f831
Muneeb-Murtaza <f2025-0987@bnu.edu.pk>
2025-11-07 12:25:59 +0500

    Add multiplication table program in C++

---
+++ Task 01.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+
+    cout << "Enter a number to print its table: ";
+    cin >> N;
+
+    cout << "\n  Multiplication Table of " << N << ":\n";
+
+    // For loop to print table from 1 to 10
+    for (int i = 1; i <= 10; i++) {
+        cout <<  N << " x " << i << " = " <<  N* i  << endl;
+    }
+
+    return 0;
+}
